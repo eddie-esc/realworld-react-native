98b95b33caf5d7bf7a9e3eebfebc47bcf9641989
Eddie Escamilla <emartinezescamilla@gmail.com>
2019-03-03 20:43:13 -0800

    feature: add nav bar buttons title and actions based off of auth selector

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,12 +3,41 @@ import { connect } from 'react-redux'
 import { Button, View, Text } from 'react-native'
 
 import { isLoggedIn } from '../redux/auth/selectors'
+import { SCREENS, COLORS, NAV_BUTTONS } from '../helpers/constants'
 
 const mapStateToProps = state => ({
     isLoggedIn: isLoggedIn(state)
 })
 
 class HomeScreen extends React.PureComponent {
+    static navigationOptions = ({ navigation, navigationOptions }) => {
+        const { params } = navigation.state
+        const rightButtonTitle = (params && params.isLoggedIn) ? NAV_BUTTONS.profile : NAV_BUTTONS.signupLogin
+        const navigateToScreen = (params && params.isLoggedIn) ? SCREENS.Settings : SCREENS.SignUp
+
+        return {
+            headerRight: (
+                <Button
+                    onPress={() => navigation.navigate(navigateToScreen)}
+                    title={rightButtonTitle}
+                    color={COLORS.green}
+                />
+            )
+        }
+    }
+
+    componentDidMount () {
+        const { isLoggedIn } = this.props
+        this.props.navigation.setParams({ isLoggedIn: isLoggedIn })
+    }
+
+    componentDidUpdate(prevProps) {
+        const { isLoggedIn } = this.props
+        if (prevProps.isLoggedIn !== isLoggedIn) {
+            this.props.navigation.setParams({ isLoggedIn: isLoggedIn })
+        }
+    }
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -20,18 +49,6 @@ class HomeScreen extends React.PureComponent {
                 <Button
                     title='Create a new article'
                     onPress={() => this.props.navigation.navigate('NewArticle')}
-                />
-                <Button
-                    title='Login to existing account'
-                    onPress={() => this.props.navigation.navigate('Login')}
-                />
-                <Button
-                    title='Signup and create a new account'
-                    onPress={() => this.props.navigation.navigate('SignUp')}
-                />
-                <Button
-                    title='Edit your profile settings'
-                    onPress={() => this.props.navigation.navigate('Settings')}
                 />
             </View>
         );
